4578304b00303e9b21c61cea046766920ab92958
pengdololololololololol <108115799+pengdol@users.noreply.github.com>
2022-09-16 17:49:10 +0900

    Create Week1.c

---
+++ Week1.c
@@ -0,0 +1,33 @@
+/*void main()
+{
+  double height[4];
+  int j = 2;
+  int i;
+
+  height[0] = 145.7;
+  height[1] = 156.3;
+  height[2] = 147.89;
+  j++;
+  height[j] = 159.4;
+  for(i = 0; i < 4; i++)
+  {
+    printf("%f ", height[i]);
+  }
+  return 0;
+}*/
+
+/*void main()
+{
+  int num[] = {5, 2, 6, 7, 8, 9, 4, 3, 1};
+  int page[10] = {123, 57, 67, 86};
+  int i, j;
+  for(i = 0; i < 10; i++)
+  {
+    printf("%d ", num[i]);
+  }
+  printf("\n");
+  for(j = 0; j < 10; j++)
+  {
+    printf("%d ", page[j]);
+  }
+}*/
